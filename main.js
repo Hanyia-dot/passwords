b598efa6f6fd77d24347d49cc17c431dd1d48676
Hanna Dobrowolska <tak.obiektyw.nie@gmail.com>
2022-06-05 13:32:42 +0200

    add variable and event listener

---
+++ main.js
@@ -0,0 +1,11 @@
+const input = document.querySelector('input');
+const passwords = ["jedEN" , "DwA"];
+const messages = ["super" , "działa!"];
+
+const showMessage = (e) => {
+
+    
+
+}
+
+input.addEventListener("input", showMessage)
